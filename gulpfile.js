79e20a1021c88b29da465c0eb9bdcfbd0578b861
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-06-25 12:14:09 +0900

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,53 @@ const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({pattern:['gulp-*']});
 
 
-gulp.task('js', function () {
+gulp.task('js', () => {
 	return gulp.src('src/js/**/*.js')
 		.pipe($.plumber())
-		.pipe($.babel({presets: ['es2015']}))
+		.pipe($.babel({presets: [['env', {targets: {ie: 11}}]]}))
 		.pipe($.uglify())
 		.pipe($.rename({extname: '.min.js'}))
 		.pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
 	return gulp.src(['src/sass/**/*.scss'], {base: 'src/sass'})
 		.pipe($.plumber())
 		.pipe($.changed('dist/sass'))
 		.pipe(gulp.dest('dist/sass'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/sass/**/*.scss', ['sass']);
+gulp.task('watch', () => {
+	gulp.watch('src/js/**/*.js', gulp.series('js'));
+	gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task('build', ['js', 'sass']);
+gulp.task('build', gulp.parallel('js', 'sass'));
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', gulp.series('build', 'watch'));
 
 
 // -----------------------------------------------------------------------------
 
-gulp.task('docs-sass', ['sass'], function () {
+gulp.task('docs-sass', gulp.series('sass', () => {
 	return gulp.src('docs/style.scss')
 		.pipe($.plumber())
 		.pipe($.sourcemaps.init())
 		.pipe($.sass({outputStyle: 'compressed'}))
-		.pipe($.autoprefixer(['ie >= 11']))
+		.pipe($.autoprefixer({browsers: ['ie >= 11'], remove: false}))
 		.pipe($.rename({extname: '.min.css'}))
 		.pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('docs'));
-});
+}));
 
-gulp.task('docs-js', ['js'], function () {
+gulp.task('docs-js', gulp.series('js', () => {
 	return gulp.src(['dist/js/stile-full.min.js'])
 		.pipe($.plumber())
 		.pipe(gulp.dest('docs'));
-});
+}));
 
-gulp.task('docs', ['default'], () => {
+gulp.task('docs', gulp.parallel('default', () => {
 	gulp.watch('src/js/**/*.js',     ['docs-js']);
 	gulp.watch('src/sass/**/*.scss', ['docs-sass']);
 	gulp.watch('docs/style.scss',    ['docs-sass']);
-});
+}));
