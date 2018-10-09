025c07ec111a76f67af53d048da68f19dfa4338c
Takty <tokutadaginaya@gmail.com>
2018-10-09 17:22:37 +0900

    Fix browser detection

--- src/js/wp/fix-edge.js
+++ src/js/wp/fix-edge.js
@@ -10,7 +10,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	if (ST.BROWSER === 'edge') {
+	const ua = window.navigator.userAgent.toLowerCase();
+	if (ua.indexOf('edge') !== -1) {
 		// Fix the image option bug which occurs when the window does not have scroll bar
 		const wpwrap = document.getElementById('wpwrap');
 		if (wpwrap) wpwrap.style.minHeight = '101%';
